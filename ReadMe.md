b896b01a9cc77b33fa9a1e0da1dafa6c54f049bb
NomadTales <nomadtales@gmail.com>
2022-02-05 15:04:04 +1100

    rework to include GTAV

--- ReadMe.md
+++ ReadMe.md
@@ -1,5 +1,5 @@
-# Suspend RDR2
-Quick script to pause and restart Red Dead Redemption 2 process on PC. Specifically used when playing Red Dead Online as it will put you into a temporary solo lobby.
+# Suspend Rockstar
+Quick script to pause and restart Grand Theft Auto V or Red Dead Redemption 2 process on PC. Specifically used when playing Grand Theft Auto Online or Red Dead Online as it will put you into a temporary solo lobby.
 
 Additionally it will create a Firewall Rule to block specific ports to extend the Solo Lobby indefinitely. The Firewall Rule can be enabled/disabled as an optional extra.
 
@@ -15,13 +15,13 @@ A once off installation of PS Tools is required.
 ### Suspend RDR2
 1. Change PowerShell Execution Policy if not previously done, by starting PowerShell as an Administrator and using `Set-ExecutionPolicy Bypass`.
 2. Create a new desktop Shortcut.
-3. Add the following to the "Type the location of the item" box: `powershell.exe -command "Invoke-Command -ScriptBlock ([ScriptBlock]::Create((Invoke-WebRequest -Uri 'https://raw.githubusercontent.com/nomadtales/SuspendRDR2/master/Suspend-RDR2.ps1' -UseBasicParsing).Content))"`.
+3. Add the following to the "Type the location of the item" box: `powershell.exe -command "Invoke-Command -ScriptBlock ([ScriptBlock]::Create((Invoke-WebRequest -Uri 'https://raw.githubusercontent.com/nomadtales/SuspendRockstar/master/Suspend-Rockstar.ps1' -UseBasicParsing).Content))"`.
 4. Call the Shortcut "RDR2 Suspend" and click *Finish*.
 5. Set the shortcut to permanently Run as Administrator by right-clicking the Shortcut, selecting *Properties*, click on *Advanced*, check the *Run as Administrator* box.
 
 ## Usage
 - When starting the script it will try and create the FireWall Rule the first time. Enter on *Y* to Confirm creation.
-- Once started either type *S* to Suspend RDR2 for 10 seconds or *F* to enable the FireWall Rule.
+- Once started either type *S* to Suspend GTAO or RDR2 for 10 seconds or *F* to enable the FireWall Rule.
 - Only Enable the FireWall Rule once you are in a Session, otherwise you will not be able to connect to a game.
 
 ![Screen Capture](ScreenCap.png)
--- Suspend-Rockstar.ps1
+++ Suspend-Rockstar.ps1
@@ -11,17 +11,17 @@ If (!$currentPrincipal.IsInRole([Security.Principal.WindowsBuiltInRole]::Adminis
 } # End If
 
 # Check if Firewall Rule exists
-$FWRule = Get-NetFirewallRule -DisplayName "RDO Solo Lobby" -ErrorAction SilentlyContinue
+$FWRule = Get-NetFirewallRule -DisplayName "Rockstar Solo Lobby" -ErrorAction SilentlyContinue
 
 # If the FireWall Rule does not exist, create it
 If (!$FWRule)
 {
     "Creating new Firewall Rule to block udp on ports 6672, 61455-61458"
-    New-NetFirewallRule -DisplayName "RDO Solo Lobby" -Direction Outbound -Action Block -Protocol UDP -RemotePort 6672,61455-61458 -Confirm
+    New-NetFirewallRule -DisplayName "Rockstar Solo Lobby" -Direction Outbound -Action Block -Protocol UDP -RemotePort 6672,61455-61458 -Confirm
 } # End If
 
 # Disable the Firewall
-Set-NetFirewallRule -DisplayName "RDO Solo Lobby" -Enabled False -ErrorAction SilentlyContinue
+Set-NetFirewallRule -DisplayName "Rockstar Solo Lobby" -Enabled False -ErrorAction SilentlyContinue
 
 # Create a loop to allow for rerunning
 While ($choice -ne 2)
@@ -30,7 +30,7 @@ While ($choice -ne 2)
     Clear-Host
 
     # Check FireWall Rule Status
-    $FWRuleStatus = (Get-NetFirewallRule -DisplayName "RDO Solo Lobby").Enabled
+    $FWRuleStatus = (Get-NetFirewallRule -DisplayName "Rockstar Solo Lobby").Enabled
 
     # Add helpful FireWall text
     if ($FWRuleStatus -eq "True")
@@ -45,11 +45,11 @@ While ($choice -ne 2)
     }
 
     # Add text for last Suspend Time
-    if ($suspendtime) {"RDO last suspended at: $suspendtime"}
+    if ($suspendtime) {"GTAO/RDO last suspended at: $suspendtime"}
 
     # prompt for rerun with a choice
     $title = 'What would you like to do?'
-    $suspend = New-Object System.Management.Automation.Host.ChoiceDescription '&Suspend RDO',"Suspend RDO for $delay seconds"
+    $suspend = New-Object System.Management.Automation.Host.ChoiceDescription '&Suspend GTAO/RDO',"Suspend GTAO/RDO for $delay seconds"
     $fwswitch = New-Object System.Management.Automation.Host.ChoiceDescription 'Enable/Disable &FireWall Rule','Start/Stop the Firewall Rule'
     $exit = New-Object System.Management.Automation.Host.ChoiceDescription 'E&xit','Disables the Firewall Rule and Exits'
     $options = [System.Management.Automation.Host.ChoiceDescription[]] ($suspend,$fwswitch,$exit)
@@ -59,8 +59,8 @@ While ($choice -ne 2)
     # Suspend RDO choice
     if ($choice -eq 0) 
     {
-        # Grab the process id of the RDR2 process
-        $ps = (Get-Process -Name RDR2 -ErrorAction SilentlyContinue).Id
+        # Grab the process id of the GTAV/RDR2 process
+        $ps = (Get-Process | Where-Object Name -match "GTAV|RDR2" -ErrorAction SilentlyContinue).Id
 
         # Suspend for delay time if process is running
         if ($ps)
@@ -72,7 +72,7 @@ While ($choice -ne 2)
         } # End If
 
         # else ignore
-        else {"$(Get-Date -Format "yyyy-MM-dd HH:mm:ss") - RDR2 Not Running"}
+        else {"$(Get-Date -Format "yyyy-MM-dd HH:mm:ss") - GTAV/RDR2 Not Running"}
     } # End ElseIf
 
     # Start or Stop FireWall Rule choice
@@ -81,14 +81,14 @@ While ($choice -ne 2)
         # Enable the Firewall
         If ($FWRuleStatus -eq "False")
         {
-            Try {Set-NetFirewallRule -DisplayName "RDO Solo Lobby" -Enabled True}
+            Try {Set-NetFirewallRule -DisplayName "GTAO/RDO Solo Lobby" -Enabled True}
             Catch {Write-Warning "Could not Enable FireWall Rule"}
         } # End If
 
         # Disable the Firewall
         If ($FWRuleStatus -eq "True")
         {
-            Try {Set-NetFirewallRule -DisplayName "RDO Solo Lobby" -Enabled False}
+            Try {Set-NetFirewallRule -DisplayName "GTAO/RDO Solo Lobby" -Enabled False}
             Catch {Write-Warning "Could not Disable FireWall Rule"}
         } # End If
     } # End ElseIf
@@ -99,7 +99,7 @@ While ($choice -ne 2)
         # Disable the Firewall
         If ($FWRuleStatus -eq "True")
         {
-            Try {Set-NetFirewallRule -DisplayName "RDO Solo Lobby" -Enabled False}
+            Try {Set-NetFirewallRule -DisplayName "GTAO/RDO Solo Lobby" -Enabled False}
             Catch {Write-Warning "Could not Disable FireWall Rule"}
         } # End If
         exit
